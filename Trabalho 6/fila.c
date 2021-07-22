4160a0129e5edc39c674fa8bf60643b7e2a3d1e0
alelekime <alessandrass11@yahoo.com>
2021-07-21 23:06:57 -0300

    Criando lista com prioridades e adcionando o valor no vetor

--- Trabalho 6/fila.c
+++ Trabalho 6/fila.c
@@ -1,35 +1,31 @@
 #include "fila.h"
 
-void inicializa_fila(filas *fila, int *prioridades)
-{
+void inicializa_fila(filas *fila, int *prioridades) {
 
     srand((unsigned)time(NULL));
-    for (int i = 1; i < MAX; i++)
-    {
+    for (int i = 0; i < MAX; i++) {
         fila->prioridade = 1 + (rand() % 5);
-        printf("%d\n", fila->prioridade);
-        prioridades[fila->prioridade - 1]++;
-        fila->prox = malloc(sizeof(fila));
-        fila = fila->prox;
+        //printf("%d\n", fila->prioridade);
+        prioridades[fila->prioridade -1]++;
+        if (i < MAX) {
+
+          fila->prox = NULL;
+
+        } else {
+            fila->prox = malloc(sizeof(fila));
+            fila = fila->prox;
+        }
     }
-    fila->prioridade = 1 + (rand() % 5);
-    prioridades[fila->prioridade]++;
-    fila->prox = NULL;
-    printf("%d\n", fila->prioridade);
 }
 
-void inicializa_prioridade(int *prioridades)
-{
-    for (int i = 1; i < 5; i++)
-    {
+void inicializa_prioridade(int *prioridades) {
+    for (int i = 1; i < 5; i++) {
         prioridades[i] = 0;
     }
 }
-void imprime_lista(int *prioridades)
-{
+void imprime_lista(int *prioridades) {
     printf("+++ FIM DO EXPEDIENTE +++ \n\nATENDIDOS:\n\n");
-    for (int i = 0; i < 5; i++)
-    {
+    for (int i = 0; i < 5; i++) {
         printf("prioridade %d: %d ", i + 1, prioridades[i]);
         printf("\n");
     }
