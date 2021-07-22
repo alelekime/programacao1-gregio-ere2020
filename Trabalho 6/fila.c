81f3b3f60a22276ffaf57ab04700ad8175790a76
alelekime <alessandrass11@yahoo.com>
2021-07-22 01:07:24 -0300

    erro vetor prioridades

--- Trabalho 6/fila.c
+++ Trabalho 6/fila.c
@@ -1,21 +1,87 @@
 #include "fila.h"
 
 void inicializa_fila(filas *fila, int *prioridades) {
-
     srand((unsigned)time(NULL));
     for (int i = 0; i < MAX; i++) {
-        fila->prioridade = 1 + (rand() % 5);
-        //printf("%d\n", fila->prioridade);
-        prioridades[fila->prioridade -1]++;
-        if (i < MAX) {
-
-          fila->prox = NULL;
-
-        } else {
-            fila->prox = malloc(sizeof(fila));
-            fila = fila->prox;
-        }
+      insere_ordenado_fila(1 + (rand() % 100), fila);
     }
+}
+
+
+int insere_ordenado_fila(int x, filas *fila){
+    filas *item, *ant = NULL, *p;
+
+    item = malloc(sizeof(filas));
+    item->valor = x;
+    item->prox = NULL;
+
+    if (fila->prox == NULL)
+    {
+        fila->prox = item;
+        return 1;
+    }
+
+    p = fila->prox;
+
+    while (p != NULL && p->valor > x)
+    {
+        ant = p;
+        p = p->prox;
+    }
+    if (ant == NULL) {
+      insere_inicio_lista(x, fila);
+
+    }
+    else{
+      item -> prox = p;
+      ant -> prox = item;
+    }
+
+    return 1;
+
+}
+
+int insere_inicio_lista(int x, filas*fila)
+{
+
+    filas *item = malloc(sizeof(filas));
+
+    item-> valor = x;
+    if (fila->prox == NULL)
+    {
+        fila->prox = item;
+        item->prox = NULL;
+    }
+
+    else
+    {
+        item->prox = fila->prox;
+        fila->prox= item;
+    }
+
+    return 1;
+
+}
+
+int insere_fim_lista(int x, filas*fila)
+{
+    filas *item, *p;
+    p = fila->prox;
+    if ((item = malloc(sizeof(filas))) != NULL)
+    {
+        item->valor = x;
+        item->prox = NULL;
+        if (fila->prox == NULL)
+            fila->prox = item;
+        else
+        {
+            while (p->prox != NULL)
+                p = p->prox;
+            p->prox = item;
+        }
+        return 1;
+    }
+    return 0;
 }
 
 void inicializa_prioridade(int *prioridades) {
--- Trabalho 6/fila.h
+++ Trabalho 6/fila.h
@@ -10,9 +10,13 @@
 typedef struct filas
 {
     int prioridade;
+    int valor;
     struct filas *prox;
 } filas;
 
 void inicializa_fila(filas *fila, int *prioridades);
 void inicializa_prioridade(int *prioridades);
+int insere_ordenado_fila(int x, filas *fila);
+int insere_inicio_lista(int x, filas*fila);
+int insere_fim_lista(int x, filas*fila);
 void imprime_lista(int *prioridades);
